8b188956961a9bb09a1b98a3058a7399f00f3860
Alex Bussey <waxidiotic@users.noreply.github.com>
2017-10-30 21:18:01 -0400

    Init main.js commit

--- demos/developer-showcase/share-to-custom-site/js/main.js
+++ demos/developer-showcase/share-to-custom-site/js/main.js
@@ -1,1 +1,25 @@
 // Add your JavaScript here
+var playerInstance = jwplayer('player');
+
+playerInstance.setup({
+  playlist: '//cdn.jwplayer.com/v2/media/xJ7Wcodt',
+  sharing: {
+    sites: [
+      'facebook',
+      'twitter',
+      {
+        icon: 'assets/whatsapp.png',
+        src: shareToWhatsApp,
+        label: 'WhatsApp'
+      }
+    ]
+  }
+});
+
+function shareToWhatsApp() {
+  // If a permalink is configured for a piece of content in the dashboard, it will share it.
+  // If not, it will fall back to sharing the current page's URL (or preview page if using a feed).
+  var linkToShare = playerInstance.getPlaylistItem().link || window.location;
+
+  window.location.href = 'https://api.whatsapp.com/send?text=' + linkToShare;
+}
